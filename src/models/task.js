3f0480ff1d4f34aa34e7940fad3ee9ccf1ff09aa
Joyce Wong <wcyjoyce.hk@gmail.com>
2020-04-05 16:51:00 +0800

    added user and task models

---
+++ src/models/task.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+    required: true
+  }
+});
+
+const task = new Task({
+  description: "First task"
+});
+
+task.save().then(() => {
+  console.log("Task created: " + task);
+}).catch(error => {
+  console.log("Task cannot be added! " + error);
+});
+
+module.exports = Task;
---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  age: {
+    type: Number,
+    required: true,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive error.");
+      };
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid.");
+      };
+    }
+  }
+});
+
+const user = new User({
+  name: "Joyce",
+  age: "26",
+  email: "joyce@gmail.com"
+});
+
+user.save().then(() => {
+  console.log("User created: " + user);
+}).catch(error => {
+  console.log("User cannot be added! " + error);
+});
+
+module.exports = User;
