c3bdafc6dcf61b7a732f14f456aab53f32b8f949
Joyce Wong <wcyjoyce.hk@gmail.com>
2020-04-06 00:07:38 +0800

    useFindandModify added to resolve deprecation issue

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,5 +3,6 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/node-tasks-api", {
   useNewUrlParser: true,
   useCreateIndex: true, // indexes are created when models are created in Mongo DB
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
