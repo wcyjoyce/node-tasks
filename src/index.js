4db223918ce4c9ea613d60beaf1789dd29d6a90f
Joyce Wong <wcyjoyce.hk@gmail.com>
2020-04-28 07:36:36 +0800

    refactoring task CRUD functions with async/await

--- src/index.js
+++ src/index.js
@@ -103,52 +103,91 @@ app.put("/users/:id", (req, res) => {
 ///// MODEL #2: TASKS /////
 
 // CRUD #1: Creating tasks
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
-  task.save().then(() => {
+  // task.save().then(() => {
+  //   res.status(201).send(task);
+  // }).catch(error => {
+  //   res.status(400).send(error);
+  // });
+
+  // Refactoring with async/await
+  try {
+    await task.save();
     res.status(201).send(task);
-  }).catch(error => {
+  } catch(error) {
     res.status(400).send(error);
-  });
+  };
 });
 
 // CRUD #2: Listing all tasks
-app.get("/tasks", (req, res) => {
-  Task.find({}).then(tasks => {
+app.get("/tasks", async (req, res) => {
+  // Task.find({}).then(tasks => {
+  //   res.send(tasks);
+  // }).catch(error => {
+  //   res.status(400).send(error);
+  // });
+
+  // Refactoring with async/await
+  try {
+    const tasks = await Task.find({});
     res.send(tasks);
-  }).catch(error => {
+  } catch(error) {
     res.status(400).send(error);
-  });
+  };
 });
 
 // CRUD #3: Listing individual task
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
 
-  Task.findById(_id).then(task => {
+  // Task.findById(_id).then(task => {
+  //   if (!task) {
+  //     return res.status(404).send();
+  //   }
+  //   res.send(task);
+  // }).catch(error => {
+  //   res.status(500).send(error);
+  // });
+
+  // Refactoring with async/await
+  try {
+    const task = await Task.findById(_id);
     if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  }).catch(error => {
+      return res.status.send(404).send();
+    } else {
+      res.send(task);
+    };
+  } catch(error) {
     res.status(500).send(error);
-  });
+  };
 });
 
 // CRUD #4: Updating a task
-app.put("/tasks/:id", (req, res) => {
+app.put("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
 
-  Task.findByIdAndUpdate(_id, { completed: true }).then(task => {
+  // Task.findByIdAndUpdate(_id, { completed: true }).then(task => {
+  //   res.send(task);
+  //   console.log(task);
+  //   return Task.countDocuments({ completed: false });
+  // }).then(result => {
+  //   console.log("Tasks outstanding: " + result);
+  // }).catch(error => {
+  //   res.send(error);
+  // });
+
+  // Refactoring with async/await
+  try {
+    const task = await Task.findByIdAndUpdate(_id, { completed: true });
+    const count = await Task.countDocuments({ completed: true })
     res.send(task);
     console.log(task);
-    return Task.countDocuments({ completed: false });
-  }).then(result => {
-    console.log("Tasks outstanding: " + result);
-  }).catch(error => {
+    return count;
+  } catch(error) {
     res.send(error);
-  });
+  };
 });
 
 // CRUD #5: Deleting a task
