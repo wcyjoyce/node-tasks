7c07981056d3e544371eec8a0e080feeb1972ad3
Joyce Wong <wcyjoyce.hk@gmail.com>
2020-05-02 01:47:17 +0800

    adding password encryption when user details are saved/updated

--- src/models/user.js
+++ src/models/user.js
@@ -1,43 +1,59 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  age: {
-    type: Number,
-    required: true,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive error.");
-      };
-    }
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid.");
-      };
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password must be at least 7 characters and cannot contain 'password.")
-      };
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    age: {
+      type: Number,
+      required: true,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be a positive error.");
+        };
+      }
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid.");
+        };
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password must be at least 7 characters and cannot contain 'password.")
+        };
+      }
     }
   }
+);
+
+// Adding middleware: validating something before user is saved/updated
+userSchema.pre("save", async function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8); // encrypting password by 8 rounds
+  };
+
+  next();
 });
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
