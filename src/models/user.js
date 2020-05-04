a02081d4398b6a69de62c56384b85daf92b03226
Joyce Wong <wcyjoyce.hk@gmail.com>
2020-05-05 07:30:50 +0800

    authentication token generated for user when logged in

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema(
   {
@@ -58,6 +59,13 @@ userSchema.statics.findByCredentials = async (email, password) => {
       return user;
     };
   };
+};
+
+// Generate authentication token method
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "hello");
+  return token;
 };
 
 // Adding middleware: validating something before user is saved/updated
--- src/routers/user.js
+++ src/routers/user.js
@@ -73,7 +73,8 @@ router.delete("/users/:id", async (req, res) => {
 router.post("/users/login", async (req,res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password);
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (error) {
     res.status(400).send();
   };
