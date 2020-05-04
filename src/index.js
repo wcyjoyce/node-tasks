f38c37210424c68010f50de424ef1aa788a756f3
Joyce Wong <wcyjoyce.hk@gmail.com>
2020-05-05 07:54:28 +0800

    middleware added to limit REST methods that users have access to

--- src/index.js
+++ src/index.js
@@ -8,6 +8,24 @@ const taskRouter = require("./routers/task.js");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Applying middleware: limits the REST paths that users have access to
+app.use((req, res, next) => {
+  if (req.method === "GET") {
+    res.send("GET requests are disabled.");
+  } else {
+    next();
+  };
+});
+
+// Applying middleware: maintenance mode limits users from POST paths
+// app.use((req, res, next) => {
+//   if (req.method === "POST") {
+//     res.status(503).send("The site is currently undergoing maintenance and will be back up and running shortly!");
+//   } else {
+//     next();
+//   };
+// });
+
 app.use(express.json()); // parse incoming JSON to an object for further handling
 
 // Connects routers
